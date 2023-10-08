98d462a05781c394ed23580e28ffa87a71356f7a
Prof. Leonardo Melo <90953714+profleonardomelo@users.noreply.github.com>
2023-10-08 15:16:24 -0300

    .

--- PROJETOS/ProjetoOfuscacaoDeCodigo/src/Principal/x.java
+++ PROJETOS/ProjetoOfuscacaoDeCodigo/src/Principal/x.java
@@ -3,35 +3,19 @@ package Principal;
 public class x {
 
     public static void main(String[] b) {
-        int c = Integer.parseInt("\u0031\u0030"); 
-        int d = Integer.parseInt("\u0035");     
-        
+        int c = Integer.parseInt("\u0031\u0030");
+        int d = Integer.parseInt("\u0035");
+
         int e = f(c, d);
-        
-        String x = "\u004F" +
-                "\u0020" +
-                "\u0072" +
-                "\u0065" +
-                "\u0073" +
-                "\u0075" +
-                "\u006C" +
-                "\u0074" +
-                "\u0061" +
-                "\u0064" +
-                "\u006F" +
-                "\u0020" +
-                "\u0064" +
-                "\u0061" +
-                "\u0020" +
-                "\u0073" +
-                "\u006F" +
-                "\u006D" +
-                "\u0061" +
-                "\u0020" +
-                "\u0065" +
-                "\u003A" +
-                "\u0020";
-        
+
+        String x = "\u004F" + "\u0020" + "\u0072" + "\u0065";
+        x += "\u0073" + "\u0075" + "\u006C" + "\u0074";
+        x += "\u0061" + "\u0064"
+                + "\u006F" + "\u0020";
+        x += "\u0064" + "\u0061" + "\u0020" + "\u0073"
+                + "\u006F" + "\u006D" + "\u0061";
+        x += "\u0020" + "\u0065" + "\u003A" + "\u0020";
+
         System.out.println(x + e);
     }
 
