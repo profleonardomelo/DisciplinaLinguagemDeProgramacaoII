8df33e678305e967cbe50fe07f4a25163443b48a
Prof. Leonardo Melo <90953714+profleonardomelo@users.noreply.github.com>
2023-10-08 03:49:09 -0300

    .

---
+++ PROJETOS/ProjetoJNI_Biblioteca_C/ProjetoJNI_Biblioteca_C/BibliotecaBasica.h
@@ -0,0 +1,19 @@
+#include <jni.h>
+
+#ifndef _Included_Principal_ProgramaJNI
+#define _Included_Principal_ProgramaJNI
+#ifdef __cplusplus
+extern "C" {
+#endif
+	/*
+	 * Class:     Principal_ProgramaJNI
+	 * Method:    exibirNativo
+	 * Signature: ()V
+	 */
+	JNIEXPORT void JNICALL Java_Principal_ProgramaJNI_exibirNativo
+	(JNIEnv*, jobject);
+
+#ifdef __cplusplus
+}
+#endif
+#endif#pragma once
